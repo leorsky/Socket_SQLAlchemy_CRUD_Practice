548aa2dbde3708db000012e9a9585498e0ab0d8a
LeoR <leorsky77@gmail.com>
2026-08-25 20:16:47 +0300

    Add server-side validation

--- src/project_new/Server/Server_Socket.py
+++ src/project_new/Server/Server_Socket.py
@@ -1,6 +1,7 @@
 import json
 import socket
 
+from project_new.src.project_new.Сhecker import checker
 
 
 HOST = 'localhost'
@@ -27,7 +28,47 @@ def main():
 
             request_json = json.loads(request)
 
+            try:
+                if not checker.verify_login(request_json['login']):
+                    raise ValueError('Логин неверный.')
 
+                if not checker.verify_password(request_json['password']):
+                    raise ValueError('Пароль неверный.')
+
+                response = {
+                    'fail': False,
+                    'description': 'Авторизация успешна'
+                }
+
+                response_json = json.dumps(response)
+
+                client_socket.sendall(response_json.encode())
+
+            except ValueError as e:
+                print(f'Ошибка: {e}')
+
+                response = {
+                    'fail': True,
+                    'description': str(e)
+                }
+
+                response_json = json.dumps(response)
+
+                client_socket.sendall(response_json.encode())
+                continue
+
+            except Exception as e:
+                print(f'Ошибка: {e}')
+
+                response = {
+                    'fail': True,
+                    'description': '500'
+                }
+
+                response_json = json.dumps(response)
+
+                client_socket.sendall(response_json.encode())
+                continue
 
 if __name__ == '__main__':
     main()
